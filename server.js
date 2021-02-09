5f10b7c8fb7ab343b9de53a8909ca1a3a5f0b96d
Ben Hopkins <bjhops17@gmail.com>
2021-02-09 14:48:56 -0800

    deploy

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workoutdb", 
+    {    
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    });
 
 app.post("/api/exercise", (req, res)=> {
     db.Exercise.create(req.body)

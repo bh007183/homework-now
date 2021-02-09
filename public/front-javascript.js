d0ebf55d06c60e7725602741c6f391dd3518eeef
Ben Hopkins <bjhops17@gmail.com>
2021-02-08 17:05:15 -0800

    added exercises

--- public/front-javascript.js
+++ public/front-javascript.js
@@ -39,8 +39,34 @@ $.ajax({
 
 })
 
+$.ajax({
+    url: "/api/get",
+    method: "get"
+}).then(res =>{
+    for(let i = 0; i < res.length; i++){
+        $(".all_exer").append(
 
+        `            
+                    <div class="one col s6 m2 card">
+                    ${res[i].name} <br>
+                    ${res[i].type} <br>
+                    ${res[i].weight} <br>
+                    ${res[i].reps} <br>
+                    ${res[i].sets} <br>
+                    ${res[i].distance} <br>
+                    ${res[i].duration} <br>
+                    <button class="delete" data-id="${res[i]._id}">Delete</button>
+                    <button data-target="modal1" class="edit modal-trigger" href="#modal1" data-id="${res[i]._id}">Edit</button>
+                    </div>
+                    
+                    
+                   
+                 `
+        )
 
+    }
+        
+})
 
 $(".workoutcreate").on("click", function(event){
     console.log("click")
